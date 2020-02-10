e9770663b747c7757b77da3f1a0b26d7e97eb521
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-02-10 14:08:06 +0100

    Create paragraph with massage which i want to get after connect with DoctorsAPI(With All Auth Roles) and also button which run connectWithDoctors function

--- Front/clinicfront/src/App.js
+++ Front/clinicfront/src/App.js
@@ -1,10 +1,18 @@
 import React, {useState} from 'react';
 
 function App() {
+    const [message, setMessage] = useState(null);
+
     return(
         <div>
+            <p>{message}</p>
+            <button onClick={() => setMessage(connectWithDoctors())}>Connect to get message</button>
         </div>
     )
+
+    function connectWithDoctors() {
+        return "Nothing";
+    }
 }
 
 export default App;
