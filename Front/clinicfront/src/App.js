69fdb5fa11443c92f6157896851907aa99e60583
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-02-11 08:45:47 +0100

    Add axios to the application, function which connectWithDoctors, also add skeleton methods which are responsible for get token by given login details and get message by given token

--- Front/clinicfront/src/App.js
+++ Front/clinicfront/src/App.js
@@ -1,17 +1,38 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 
+
+// If you want to run this application you must download axios by this command:
+// npm install axios --save
 function App() {
     const [message, setMessage] = useState(null);
 
     return(
         <div>
             <p>{message}</p>
-            <button onClick={() => setMessage(connectWithDoctors())}>Connect to get message</button>
+            <button onClick={() => connectWithDoctors()}>
+                Connect to get message
+            </button>
         </div>
-    )
+    );
 
     function connectWithDoctors() {
-        return "Nothing";
+        try {
+
+
+            setMessage("Nothing");
+        }catch (e) {
+            console.log(e);
+            setMessage("Nothing");
+        }
+    }
+
+    function getTokenByGivenData(){
+
+    }
+
+    function getInfoByGivenToken(){
+
     }
 }
 
