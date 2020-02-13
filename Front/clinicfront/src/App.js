0e34574ad9bad1fa306face73f17bed1f2b44707
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-02-13 11:51:27 +0100

    Create new component pages

--- Front/clinicfront/src/App.js
+++ Front/clinicfront/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 // If you want to run this application you must download axios by this command:
@@ -6,25 +6,57 @@ import axios from 'axios';
 function App() {
     const [message, setMessage] = useState(null);
 
+    useEffect(() => {
+        const connect = async () => {
+            const URL = 'http://localhost:8762/auth';
+            const user = "jan";
+            const pass = "12345";
+            try {
+                const response = await axios({
+                    method: 'POST',
+                    url: URL,
+                    async: true,
+                    crossDomain: true,
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json;charset=UTF-8',
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    body: {
+                        "username": user,
+                        "password": pass
+                    }
+                }).then(respone => {
+                    console.log(respone.status)
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        connect();
+    }, []);
+
     return(
         <div>
-            <button></button>
-            <button></button>
-            <button></button>
+            <button onClick={() => connectWithDoctors()}>Patient</button>
+            <button>Doctor</button>
+            <button>Assistan</button>
         </div>
     );
 
-    // function connectWithDoctors() {
-    //     try {
-    //         getTokenByGivenLoginDetails();
-    //         // let givenMessage = getInfoByGivenToken();
-    //         // setMessage(tokenForLogin);
-    //     }catch (e) {
-    //         console.log(e);
-    //         setMessage("Nothing");
-    //     }
-    // }
-    //
+    function connectWithDoctors() {
+        try {
+            // let givenMessage = getInfoByGivenToken();
+            // setMessage(tokenForLogin);
+        }catch (e) {
+            console.log(e);
+            setMessage("Nothing");
+        }
+    }
+
+
+
+
     // async function getTokenByGivenLoginDetails(
     //     user = 'jan',
     //     pass = '12345'
@@ -41,10 +73,27 @@ function App() {
     //             'Access-Control-Allow-Origin': '*'
     //         },
     //         body: {
-    //             username: user,
-    //             password: pass
+    //             "username": user,
+    //             "password": pass
     //         }
     //     }).then(respone => {console.log(respone.status)});
+    // }
+
+    // async function getTokenByGivenLoginDetails(
+    //     user = 'jan',
+    //     pass = '12345'
+    // ){
+    //     const response = fetch('http://localhost:8762/auth', {
+    //         method: 'POST',
+    //         headers: {
+    //             "Access-Control-Allow-Origin": "*"
+    //         },
+    //         body: {
+    //             "username": user,
+    //             "password": pass
+    //         }
+    //     });
+    //     console.log(response);
     // }
     //
     // function getInfoByGivenToken(){
