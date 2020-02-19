f91173a576229a5faacbe86e860fa336205f4a87
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-02-19 15:23:15 +0100

    temporary Success, even if frontend will send the wrong UUID, ZUUL will change the wrong into proper from JWT

--- Zuul-MSSC/src/main/java/com/clinics/zuul/security/DoctorUUIDChecker.java
+++ Zuul-MSSC/src/main/java/com/clinics/zuul/security/DoctorUUIDChecker.java
@@ -18,6 +18,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 import static com.netflix.zuul.context.RequestContext.getCurrentContext;
 import static org.springframework.util.ReflectionUtils.rethrowRuntimeException;
@@ -111,32 +112,36 @@ public class DoctorUUIDChecker extends ZuulFilter implements JwtProperties {
 	public Object run() {
 		log.warn("DoctorUUIDChecker ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		log.warn(String.valueOf(authentication.getCredentials()));
+		log.warn(String.valueOf(((HashMap<String, Object>)authentication.getCredentials()).get("UUID")));
+		String uuidFromAuthentication = (String) ((HashMap<String, Object>) authentication.getCredentials()).get("UUID");
 //		int uuid = authentication.getCredentials();
 
 		try {
 			RequestContext context = getCurrentContext();
-//			InputStream in = (InputStream) context.get("requestEntity");
-//			if (in == null) {
-//				in = context.getRequest().getInputStream();
-//			}
-//			InputStream in = context.getRequest().getInputStream();
 			InputStream in = (InputStream) context.get("requestEntity");
+			if (in == null) {
+				in = context.getRequest().getInputStream();
+			}
+//			InputStream in = context.getRequest().getInputStream();
+//			InputStream in = (InputStream) context.get("requestEntity");
 			String body = StreamUtils.copyToString(in, StandardCharsets.UTF_8);
 			log.warn(body);
 //			String header = context.getRequest().getHeader(TOKEN_REQUEST_HEADER).replace(TOKEN_PREFIX, "");
 //			System.out.println(header + " <------------------header");
 //			log.warn(authentication.getName());
-			 body = "request body modified via set('requestEntity'): "+ body;
-			body = "{\n" +
-					"\t\"firstName\": \"123\",\n" +
-					"\t\"lastName\": \"abc\",\n" +
-					"\t\"photoUrl\": \"asdfasdfasdfasdfasdfasdfasdfasdfasdf\",\n" +
-					"\t\"licence\": \"asdf23 rrasfd\",\n" +
-					"\t\"uuid\":\"505604a5-d8af-41d1-a6b7-0f796c4d7777\"" +
-					"}";
+//			 body = "request body modified via set('requestEntity'): "+ body;
+//			body = "{\n" +
+//					"\t\"firstName\": \"123\",\n" +
+//					"\t\"lastName\": \"abc\",\n" +
+//					"\t\"photoUrl\": \"asdfasdfasdfasdfasdfasdfasdfasdfasdf\",\n" +
+//					"\t\"licence\": \"asdf23 rrasfd\",\n" +
+//					"\t\"uuid\":\"505604a5-d8af-41d1-a6b7-0f796c4d7777\"" +
+//					"}";
 			//todo throw excepiton if "uuid" not exist
-//			body.replaceAll("(\"uuid\"(\\s*):(\\s*)\")(.)*\"", "\"uuid: \"" + ");
+			log.warn(body);
+			body = body.replaceAll("(\"uuid\"(\\s*):(\\s*)\")(.)*\"", "\"uuid\": \"" +
+					uuidFromAuthentication + "\"");
+			log.warn(body);
 //			context.set(String.valueOf(new ByteArrayInputStream(body.getBytes(StandardCharsets.UTF_8))));
 			context.set("requestEntity", new ByteArrayInputStream(body.getBytes("UTF-8")));
 		}
