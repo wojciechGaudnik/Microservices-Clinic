0f41a932a2a7588fdd52ce18d138f48b5c7eecb5
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-02-21 23:08:43 +0100

    Refactor: delete some unnecessary functions from setLoginPage

--- Front/clinicfront/src/containers/SetFirstLoginPage.js
+++ Front/clinicfront/src/containers/SetFirstLoginPage.js
@@ -2,17 +2,11 @@ import {connect} from "react-redux";
 import LoginPage from "../components/LoginPage";
 import {setStoreUserDetails} from "../actions";
 
-const getMessage = state => ( state.info.userDetails );
-
-const mapStateToProps = state => ({
-    result: getMessage(state),
-});
-
 const mapDispatchToProps = dispatch => ({
         setStoreUserDetails: (userDetails) => {dispatch(setStoreUserDetails(userDetails))}
 });
 
 export default connect(
-    mapStateToProps,
+    null,
     mapDispatchToProps
 )(LoginPage)
