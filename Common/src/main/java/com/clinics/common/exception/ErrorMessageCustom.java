b6ab696a589b171794756ce54fc650644b448fab
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-02-24 21:41:15 +0100

    refactor common

--- Common/src/main/java/com/clinics/common/exception/ErrorMessageCustom.java
+++ Common/src/main/java/com/clinics/common/exception/ErrorMessageCustom.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.WordUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.context.request.ServletWebRequest;
