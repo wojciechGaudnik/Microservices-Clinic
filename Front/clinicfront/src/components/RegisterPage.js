01f4a8b82ca37f23386ca0234dbb332805b4ac63
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-02-27 10:07:26 +0100

    Change old fetch request to new universal function

--- Front/clinicfront/src/components/RegisterPage.js
+++ Front/clinicfront/src/components/RegisterPage.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {Button, Col, Form} from "react-bootstrap";
 import {useFormFields} from "../containers/SetRegisterPage"
-import {registerNewUser} from "../actions";
+import {sendRequestByGivenDetails} from "../actions";
+
 
 export const RegisterPage = () => {
     const [userRegisterInformation, setUserRegisterInformation] = useFormFields({
@@ -32,13 +33,55 @@ export const RegisterPage = () => {
         width: '60%',
     };
 
+    //Fetch packages
+    const specialFunctionSendRequestByGivenDetails = (responseJSONData) => {
+        const body = (userRegisterInformation) => ({
+            doctoruuid: responseJSONData.uuid,
+            firstName: userRegisterInformation.firstName,
+            lastName: userRegisterInformation.lastName,
+            photoUrl: userRegisterInformation.photoURL,
+            licence: userRegisterInformation.licence
+        });
+
+        const headers = {
+            'Authorization': responseJSONData.token,
+            'Content-Type': 'application/json;charset=UTF-8',
+        };
+
+        sendRequestByGivenDetails(
+            "http://localhost:8762/doctor-mssc/doctors/",
+            'POST',
+            body(userRegisterInformation),
+            headers,
+            null,
+            null
+        );
+    };
+
+    const fetchRequest = () => {
+        const body = (userRegisterInformation) => ({
+            "email": userRegisterInformation.email,
+            "password": userRegisterInformation.password,
+            "role": userRegisterInformation.role
+        });
+
+        sendRequestByGivenDetails(
+            "http://localhost:8762/auth/users/",
+            'POST',
+            body(userRegisterInformation),
+            {'Content-Type': 'application/json'},
+            null,
+            specialFunctionSendRequestByGivenDetails
+        );
+    };
+
     //Main HTML return
     return(
         <div style={styleForMainDiv}>
             <Form
                 onSubmit={e => {
                     e.preventDefault();
-                    registerNewUser(userRegisterInformation);
+                    fetchRequest();
                 }}
                 style={styleForForm}
             >
