b718771ba298426278af42b09494ce85023531ae
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-02-28 10:25:06 +0100

    Change state in from component, it cause it is independent and get only fetch request in parameter

--- Front/clinicfront/src/components/FormForInputUserInformation.js
+++ Front/clinicfront/src/components/FormForInputUserInformation.js
@@ -1,158 +1,96 @@
 import React from "react";
 
-import {redirectByRole} from "../actions";
-import {sendFetchRequestRegisterNewDoctor} from "../containers/SetRegisterPage";
+import {redirectByRole, useFormFields} from "../actions";
 
 import {Button, Col, Form} from "react-bootstrap";
 import {styleForForm, styleForFormLabel} from "../containers/SetFormForInputUserInformation";
 
 
 export const FormForInputUserInformation = (props) => {
+    const [userInformation, setUserInformation] = useFormFields({
+        firstName:  "",
+        lastName:   "",
+        licence:    "",
+        photoURL:   "",
+        email:      "",
+        password:   "",
+        role:       "doctor"
+    });
+
     const handleChange = (event) => {
         console.log(event.target.value);
-        props.setFunction(event);
+        setUserInformation(event);
     };
 
-    if (props.variant === "register"){
-        return (
-            <Form
-                onSubmit={e => {
-                    e.preventDefault();
-                    sendFetchRequestRegisterNewDoctor(props.userInformation);
-                    redirectByRole(null, props)
-                }}
-                style={styleForForm}
-            >
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label style={styleForFormLabel} >Email</Form.Label>
-                        <Form.Control type="email"
-                                      onChange={(e) => handleChange(e)}
-                                      placeholder="Email"
-                                      name="email" />
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridPassword">
-                        <Form.Label style={styleForFormLabel} >Password</Form.Label>
-                        <Form.Control type="password"
-                                      onChange={(e) => handleChange(e)}
-                                      placeholder="Password"
-                                      name="password" />
-                    </Form.Group>
-                </Form.Row>
-                <Form.Row>
-                    <Form.Group as={Col}>
-                        <Form.Label style={styleForFormLabel} >Role</Form.Label>
-                        <Form.Control onChange={(e) => handleChange(e)}
-                                      placeholder="Role"
-                                      name="role"
-                                      as="select">
-                            <option value="doctor">doctor</option>
-                            <option value="patient">patient</option>
-                            <option value="assistant">assistant</option>
-                        </Form.Control>
-                    </Form.Group>
-                    <Form.Group as={Col}>
-                        <Form.Label style={styleForFormLabel} >First Name</Form.Label>
-                        <Form.Control onChange={(e) => handleChange(e)}
-                                      placeholder="firstName"
-                                      name="firstName" />
-                    </Form.Group>
-                    <Form.Group as={Col}>
-                        <Form.Label style={styleForFormLabel} >Last Name</Form.Label>
-                        <Form.Control onChange={(e) => handleChange(e)}
-                                      placeholder="lastName"
-                                      name="lastName" />
-                    </Form.Group>
-                </Form.Row>
-                <Form.Row>
-                    <Form.Group as={Col} >
-                        <Form.Label style={styleForFormLabel} >Licence</Form.Label>
-                        <Form.Control onChange={(e) => handleChange(e)}
-                                      placeholder="Licence"
-                                      name="licence" />
-                    </Form.Group>
-                    <Form.Group as={Col} >
-                        <Form.Label style={styleForFormLabel} >PhotoURL</Form.Label>
-                        <Form.Control onChange={(e) => handleChange(e)}
-                                      placeholder="PhotoURL"
-                                      name="photoURL" />
-                    </Form.Group>
-                </Form.Row>
-                <Button variant="light" type="submit">
-                    Register
-                </Button>
-            </Form>
-        )
-    }else if(props.variant === "doctor"){
-        // return (
-        //     <Form
-        //     onSubmit={e => {
-        //         e.preventDefault();
-        //         sendFetchRequestRegisterNewDoctor(props.userInformation);
-        //         redirectByRole(null, props)
-        //     }}
-        //     style={styleForForm}
-        // >
-        //     <Form.Row>
-        //         <Form.Group as={Col} controlId="formGridEmail">
-        //             <Form.Label style={styleForFormLabel} >Email</Form.Label>
-        //             <Form.Control type="email"
-        //                           onChange={(e) => handleChange(e)}
-        //                           placeholder="Email"
-        //                           name="email" />
-        //         </Form.Group>
-        //         <Form.Group as={Col} controlId="formGridPassword">
-        //             <Form.Label style={styleForFormLabel} >Password</Form.Label>
-        //             <Form.Control type="password"
-        //                           onChange={(e) => handleChange(e)}
-        //                           placeholder="Password"
-        //                           name="password" />
-        //         </Form.Group>
-        //     </Form.Row>
-        //     <Form.Row>
-        //         <Form.Group as={Col}>
-        //             <Form.Label style={styleForFormLabel} >Role</Form.Label>
-        //             <Form.Control onChange={(e) => handleChange(e)}
-        //                           placeholder="Role"
-        //                           name="role"
-        //                           as="select">
-        //                 <option value="doctor">doctor</option>
-        //                 <option value="patient">patient</option>
-        //                 <option value="assistant">assistant</option>
-        //             </Form.Control>
-        //         </Form.Group>
-        //         <Form.Group as={Col}>
-        //             <Form.Label style={styleForFormLabel} >First Name</Form.Label>
-        //             <Form.Control onChange={(e) => handleChange(e)}
-        //                           placeholder="firstName"
-        //                           name="firstName" />
-        //         </Form.Group>
-        //         <Form.Group as={Col}>
-        //             <Form.Label style={styleForFormLabel} >Last Name</Form.Label>
-        //             <Form.Control onChange={(e) => handleChange(e)}
-        //                           placeholder="lastName"
-        //                           name="lastName" />
-        //         </Form.Group>
-        //     </Form.Row>
-        //     <Form.Row>
-        //         <Form.Group as={Col} >
-        //             <Form.Label style={styleForFormLabel} >Licence</Form.Label>
-        //             <Form.Control onChange={(e) => handleChange(e)}
-        //                           placeholder="Licence"
-        //                           name="licence" />
-        //         </Form.Group>
-        //         <Form.Group as={Col} >
-        //             <Form.Label style={styleForFormLabel} >PhotoURL</Form.Label>
-        //             <Form.Control onChange={(e) => handleChange(e)}
-        //                           placeholder="PhotoURL"
-        //                           name="photoURL" />
-        //         </Form.Group>
-        //     </Form.Row>
-        //     <Button variant="light" type="submit">
-        //         Register
-        //     </Button>
-        // </Form>)
-    }
+    return (
+        <Form
+            onSubmit={e => {
+                e.preventDefault();
+                props.sendFetchRequest(userInformation);
+                redirectByRole(null, props)
+            }}
+            style={styleForForm}
+        >
+            <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Label style={styleForFormLabel}>Email</Form.Label>
+                    <Form.Control type="email"
+                                  onChange={(e) => handleChange(e)}
+                                  placeholder="Email"
+                                  name="email"/>
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Label style={styleForFormLabel}>Password</Form.Label>
+                    <Form.Control type="password"
+                                  onChange={(e) => handleChange(e)}
+                                  placeholder="Password"
+                                  name="password"/>
+                </Form.Group>
+            </Form.Row>
+            <Form.Row>
+                <Form.Group as={Col}>
+                    <Form.Label style={styleForFormLabel}>Role</Form.Label>
+                    <Form.Control onChange={(e) => handleChange(e)}
+                                  placeholder="Role"
+                                  name="role"
+                                  as="select">
+                        <option value="doctor">doctor</option>
+                        <option value="patient">patient</option>
+                        <option value="assistant">assistant</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group as={Col}>
+                    <Form.Label style={styleForFormLabel}>First Name</Form.Label>
+                    <Form.Control onChange={(e) => handleChange(e)}
+                                  placeholder="firstName"
+                                  name="firstName"/>
+                </Form.Group>
+                <Form.Group as={Col}>
+                    <Form.Label style={styleForFormLabel}>Last Name</Form.Label>
+                    <Form.Control onChange={(e) => handleChange(e)}
+                                  placeholder="lastName"
+                                  name="lastName"/>
+                </Form.Group>
+            </Form.Row>
+            <Form.Row>
+                <Form.Group as={Col}>
+                    <Form.Label style={styleForFormLabel}>Licence</Form.Label>
+                    <Form.Control onChange={(e) => handleChange(e)}
+                                  placeholder="Licence"
+                                  name="licence"/>
+                </Form.Group>
+                <Form.Group as={Col}>
+                    <Form.Label style={styleForFormLabel}>PhotoURL</Form.Label>
+                    <Form.Control onChange={(e) => handleChange(e)}
+                                  placeholder="PhotoURL"
+                                  name="photoURL"/>
+                </Form.Group>
+            </Form.Row>
+            <Button variant="light" type="submit">
+                Register
+            </Button>
+        </Form>
+    );
 };
 
 export default FormForInputUserInformation;
--- Front/clinicfront/src/components/RegisterPage.js
+++ Front/clinicfront/src/components/RegisterPage.js
@@ -1,27 +1,18 @@
 import React from "react";
 
-import {useFormFields} from "../actions";
-
-import {styleForMainDiv} from "../containers/SetRegisterPage";
+import {
+    styleForMainDiv,
+    sendFetchRequestRegisterNewDoctor
+    } from "../containers/SetRegisterPage";
 
 import {FormForInputUserInformation} from "./FormForInputUserInformation";
 
 
 export const RegisterPage = (props) => {
-    const [userRegisterInformation, setUserRegisterInformation] = useFormFields({
-        firstName:  "",
-        lastName:   "",
-        licence:    "",
-        photoURL:   "",
-        email:      "",
-        password:   "",
-        role:       "doctor"
-    });
-
     //Main HTML return
     return(
         <div style={styleForMainDiv}>
-            <FormForInputUserInformation {...props} setFunction={setUserRegisterInformation} userInformation = {userRegisterInformation} variant="register"/>
+            <FormForInputUserInformation {...props} sendFetchRequest={sendFetchRequestRegisterNewDoctor}/>
         </div>
 )};
 
