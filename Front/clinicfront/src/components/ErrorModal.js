b306f284dbcb19358c56df09c1921126709d9d9c
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-02-29 12:21:59 +0100

    Change modal look

--- Front/clinicfront/src/components/ErrorModal.js
+++ Front/clinicfront/src/components/ErrorModal.js
@@ -1,15 +1,23 @@
 import React, {useState} from "react";
-import {Modal} from "react-bootstrap";
+import {Container, Modal} from "react-bootstrap";
 
 export const ErrorModal = () => {
     const [showErrorModal, setShowErrorModal] = useState(true);
 
+    const styleForModalContainer = {
+        background: 'red',
+        borderRadius: '7px',
+        border: '2px solid black'
+    };
+
     return (
-        <Modal show={showErrorModal} onHide={() => setShowErrorModal(false)}>
-            <Modal.Header closeButton>
-                <Modal.Title>Something goes wrong!</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Wrong login details</Modal.Body>
+        <Modal show={showErrorModal} onHide={() => setShowErrorModal(false)} >
+            <Container style={styleForModalContainer}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Something goes wrong!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Wrong login details</Modal.Body>
+            </Container>
         </Modal>
     )
 };
