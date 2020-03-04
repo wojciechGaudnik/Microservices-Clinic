e313c4ee0c9e891e9f6e6ed5a624eb1e1c171e3c
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-03-04 22:32:03 +0100

    remove front from backend

--- Front/clinicfront/src/containers/EmployeePage/PatientPage.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const PatientPage = () => (<p>Patient</p>);
--- Front/clinicfront/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import info from "./info";
-
-export default combineReducers({
-    info,
-});
