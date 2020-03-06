a73b7b8303ca218152feb1030c4992bebae83827
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-03-06 16:52:29 +0100

    Add new URL for check localstorage.token

--- Front/clinicfront/src/URLs.js
+++ Front/clinicfront/src/URLs.js
@@ -4,4 +4,5 @@ export const URLs = {
     LOGIN_USER: "http://localhost:8762/auth/login",
     GET_USER_INFORMATION: "http://localhost:8762/doctor-mssc/doctors/",
     CHANGE_DOCTOR_INFORMATION: "http://localhost:8762/doctor-mssc/doctors/",
+    GET_UUID_BY_TOKEN: "http://localhost:8762/auth/users/uuid/",
 };
