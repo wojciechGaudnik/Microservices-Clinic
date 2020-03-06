e128ac73844d78dac82767d81fd861f5dbcb9559
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-03-06 17:02:26 +0100

    Create LogOutButton.js for logout user

---
+++ Front/clinicfront/src/components/AdditionalComponents/LogOutButton.js
@@ -0,0 +1,14 @@
+import {Button} from "react-bootstrap";
+import React from "react";
+import {redirectByRole} from "../../actions";
+
+export const LogOutButton = (props) => {
+    return (
+        <Button onClick={() => {
+            localStorage.removeItem("token");
+            redirectByRole(null, props);
+        }}>
+            Log Out
+        </Button>
+    )
+};
