de814409cff2f6f53d5a2704b78fc8204cd89eb3
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-03-09 10:51:25 +0100

    Add spinners when user info is loading

--- Front/clinicfront/src/components/AdditionalComponents/ContainerForUserInformation/ContainerForUserInformation.js
+++ Front/clinicfront/src/components/AdditionalComponents/ContainerForUserInformation/ContainerForUserInformation.js
@@ -5,7 +5,7 @@ import {
     styleForRow,
     styleForValueCol
 } from "./Containers/SetContainerForUserInfo";
-import {Badge, Col, Container, Row} from "react-bootstrap";
+import {Badge, Col, Container, Row, Spinner} from "react-bootstrap";
 
 export const ContainerForUserInformation = (props) => {
     const [userInformation, setUserInformation] = useState({});
@@ -41,12 +41,36 @@ export const ContainerForUserInformation = (props) => {
     return(
         <Container style={styleForContainer}>
             <Row><h5><Badge variant="primary">{props.titleRole}</Badge></h5></Row>
-            {props.firstName        ? (<Row style={styleForRow}><Col xs={3} style={styleForKeyCol}>FirstName:</Col>      <Col style={styleForValueCol}>{userInformation.firstName}</Col>      </Row>) : null}
-            {props.lastName         ? (<Row style={styleForRow}><Col xs={3} style={styleForKeyCol}>LastName:</Col>       <Col style={styleForValueCol}>{userInformation.lastName}</Col>       </Row>) : null}
-            {props.licence          ? (<Row style={styleForRow}><Col xs={3} style={styleForKeyCol}>Licence:</Col>        <Col style={styleForValueCol}>{userInformation.licence}</Col>        </Row>) : null}
-            {props.calendars        ? (<Row style={styleForRow}><Col xs={3} style={styleForKeyCol}>Calendars:</Col>      <Col style={styleForValueCol}>{displayCalendars()}</Col>             </Row>) : null}
-            {props.specializations  ? (<Row style={styleForRow}><Col xs={3} style={styleForKeyCol}>Specialization:</Col> <Col style={styleForValueCol}>{displaySpecializations()}</Col>       </Row>) : null}
-            {props.medicalUnits     ? (<Row style={styleForRow}><Col xs={3} style={styleForKeyCol}>Medical Units:</Col>  <Col style={styleForValueCol}>{userInformation.medicalUnits}</Col>   </Row>) : null}
+            {props.firstName        ? (
+                <Row style={styleForRow}>
+                    <Col xs={3} style={styleForKeyCol}>FirstName:</Col>
+                    <Col style={styleForValueCol}>{userInformation.firstName ? (userInformation.firstName) : (<Spinner animation="border" size="sm" />)}</Col>
+                </Row>) : null}
+            {props.lastName         ? (
+                <Row style={styleForRow}>
+                    <Col xs={3} style={styleForKeyCol}>LastName:</Col>
+                    <Col style={styleForValueCol}>{userInformation.lastName ? (userInformation.lastName) : (<Spinner animation="border" size="sm" />)}</Col>
+                </Row>) : null}
+            {props.licence          ? (
+                <Row style={styleForRow}>
+                    <Col xs={3} style={styleForKeyCol}>Licence:</Col>
+                    <Col style={styleForValueCol}>{userInformation.licence ? (userInformation.licence) : (<Spinner animation="border" size="sm" />)}</Col>
+                </Row>) : null}
+            {props.calendars        ? (
+                <Row style={styleForRow}>
+                    <Col xs={3} style={styleForKeyCol}>Calendars:</Col>
+                    <Col style={styleForValueCol}>{displayCalendars() ? (userInformation.calendars) : (<Spinner animation="border" size="sm" />)}</Col>
+                </Row>) : null}
+            {props.specializations  ? (
+                <Row style={styleForRow}>
+                    <Col xs={3} style={styleForKeyCol}>Specialization:</Col>
+                    <Col style={styleForValueCol}>{displaySpecializations() ? (userInformation.specializations) : (<Spinner animation="border" size="sm" />)}</Col>
+                </Row>) : null}
+            {props.medicalUnits     ? (
+                <Row style={styleForRow}>
+                    <Col xs={3} style={styleForKeyCol}>Medical Units:</Col>
+                    <Col style={styleForValueCol}>{userInformation.medicalUnits}</Col>
+                </Row>) : null}
         </Container>
     )
 };
