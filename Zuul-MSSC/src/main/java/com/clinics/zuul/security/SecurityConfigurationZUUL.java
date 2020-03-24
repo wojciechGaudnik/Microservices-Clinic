ddede0fb37907d2bc54ca71b50bee281befdc527
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-03-24 20:08:02 +0100

    added swagger into doctor and medical unites

--- Zuul-MSSC/src/main/java/com/clinics/zuul/security/SecurityConfigurationZUUL.java
+++ Zuul-MSSC/src/main/java/com/clinics/zuul/security/SecurityConfigurationZUUL.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -21,10 +20,19 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class SecurityConfigurationZUUL extends WebSecurityConfigurerAdapter implements Role, JwtProperties {
 
+	private static final String[] SWAGGER_WHITE_LIST = {
+			"/swagger-resources/**",
+			"/swagger-ui.html",
+			"/v2/api-docs",
+			"/webjars/**"
+	};
+
 	//todo 401 Unauthorized – This means the user isn’t not authorized to access a resource. It usually returns when the user isn’t authenticated.
 	//todo 403 Forbidden – This means the user is authenticated, but it’s not allowed to access a resource.
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
+//		http.authorizeRequests().antMatchers("/**").permitAll();
 		http
 				.cors().and()
 				.csrf().disable()
@@ -33,7 +41,14 @@ public class SecurityConfigurationZUUL extends WebSecurityConfigurerAdapter impl
 				.exceptionHandling().authenticationEntryPoint((request, response, e) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
 				.and()
 				.addFilterAfter(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+
 				.authorizeRequests()
+
+				.antMatchers("/doctor-mssc/v2/api-docs/**").permitAll()
+				.antMatchers("/medical-units-mssc/v2/api-docs/**").permitAll()
+				.antMatchers("/auth/v2/api-docs/**").permitAll()
+				.antMatchers(SWAGGER_WHITE_LIST).permitAll()
+
 
 				.antMatchers(HttpMethod.GET,"/doctor-mssc/specializations").hasAnyRole(Role.DOCTOR)
 				.antMatchers(HttpMethod.POST,"/doctor-mssc/specializations").hasAnyRole(Role.DOCTOR)
@@ -42,12 +57,13 @@ public class SecurityConfigurationZUUL extends WebSecurityConfigurerAdapter impl
 				.antMatchers(HttpMethod.GET,"/doctor-mssc/specializations").hasAnyRole(Role.DOCTOR)
 				.antMatchers(HttpMethod.GET, "/doctor-mssc/doctors/test/**").permitAll()
 				.antMatchers(HttpMethod.POST, "/doctor-mssc/doctors/").hasAnyRole(Role.DOCTOR)
-				.antMatchers(HttpMethod.GET, "/doctor-mssc/doctors/test/**").access("@userUUIDChecker.checkUserUUID(authentication, #uuid)")
 				.antMatchers(HttpMethod.GET, "/doctor-mssc/doctors/{uuid}").access("@userUUIDChecker.checkUserUUID(authentication, #uuid)")
 				.antMatchers("/doctor-mssc/doctors/{uuid}/**").access("@userUUIDChecker.checkUserUUID(authentication, #uuid)")
 				.antMatchers(HttpMethod.DELETE, "/doctor-mssc/doctors/{uuid}").access("@userUUIDChecker.checkUserUUID(authentication, #uuid)")
 				.antMatchers(HttpMethod.PUT, "/doctor-mssc/doctors/{uuid}").access("@userUUIDChecker.checkUserUUID(authentication, #uuid)")
 				.antMatchers(HttpMethod.PATCH, "/doctor-mssc/doctors/{uuid}").access("@userUUIDChecker.checkUserUUID(authentication, #uuid)")
+
+
 
 				.antMatchers(HttpMethod.POST, "/patient-mssc/patients").hasAnyRole(Role.PATIENT)
 				.antMatchers(HttpMethod.POST, "/patient-mssc/patients/{patientUUID}/visit").access("@userUUIDChecker.checkUserUUID(authentication, #patientUUID)")
