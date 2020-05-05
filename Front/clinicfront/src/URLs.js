ee5ca1a60ae8e67ce502bfca36011fe1ea29afd0
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-05-05 19:29:43 +0200

    Change "" to `` and ${}

--- Front/clinicfront/src/URLs.js
+++ Front/clinicfront/src/URLs.js
@@ -1,8 +1,10 @@
+
 export const URLs = {
     REGISTER_USER:                  "http://localhost:8762/auth/users/",
     REGISTER_PATIENT:               "http://localhost:8762/patient-mssc/patients",
     REGISTER_DOCTOR:                "http://localhost:8762/doctor-mssc/doctors/",
     GET_DOCTOR_INFORMATION:         "http://localhost:8762/doctor-mssc/doctors/",
+    GET_ALL_DOCTORS:                "http://localhost:8762/doctor-mssc/doctors",
     GET_PATIENT_INFORMATION:        "http://localhost:8762/patient-mssc/patients/",
     GET_DETAILS_BY_TOKEN:           "http://localhost:8762/auth/users/uuidAndRole/",
     LOGIN_USER:                     "http://localhost:8762/auth/login",
@@ -12,11 +14,9 @@ export const URLs = {
     DELETE_PATIENT:                 "",
     GET_ALL_DOCTOR_CALENDARS: (
             doctorUUID
-        ) => (                      "http://localhost:8762/doctor-mssc/doctors/" + doctorUUID + "/calendars"),
+        ) => (                      `http://localhost:8762/doctor-mssc/doctors/${doctorUUID}/calendars`),
     GET_ALL_APPOINTMENTS_IN_GIVEN_CALENDAR : (
             doctorUUID,
             calendarUUID
-        ) => (                      "http://localhost:8762/doctor-mssc/doctors/" + doctorUUID + "/calendars/" + calendarUUID + "/appointments")
+        ) => (                      `http://localhost:8762/doctor-mssc/doctors/${doctorUUID}/calendars/${calendarUUID}/appointments1`)
 };
-
-export const PATHs = {};
