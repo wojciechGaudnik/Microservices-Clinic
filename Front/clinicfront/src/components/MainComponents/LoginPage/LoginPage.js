3d5b8e32fe438edd331983dfbf840f0ab89afb91
Wojciech Adamowski <wojtekadam1@gmail.com>
2020-05-07 10:39:47 +0200

    Refactor states and fetch requests

--- Front/clinicfront/src/components/MainComponents/LoginPage/LoginPage.js
+++ Front/clinicfront/src/components/MainComponents/LoginPage/LoginPage.js
@@ -1,65 +1,139 @@
-import React, {useEffect, useState} from "react";
-
-import {sendFetchRequestIsThereLoginUser, sendFetchRequestLoginUser} from "./SetLoginPage";
+import React, {useEffect, useReducer} from "react";
 
 import {redirectByRole} from "../../../actions";
 
 import {FormForInputUserInformation} from "../../AdditionalComponents/FormForInputUserInfo/FormForInputUserInformation";
 import {Container} from "@material-ui/core";
 import AlertMessage from "../../AdditionalComponents/Alert/AlertMessage";
-
+import {URLs} from "../../../URLs";
 
 export const LoginPage = (props) => {
-    const { error, setStoreError, setStoreUserDetails } = props;
-    const [showWarningMessage, setShowWarningMessage] = useState(error["isError"]);
-    const [userDetails, setUserDetails] = useState({
-        uuid: null,
-        role: null
-    });
 
-    //Effects after each render
-    useEffect(() => {
-        setStoreUserDetails(userDetails);
-        console.log(userDetails);
-        if (userDetails.role){redirectByRole(userDetails.role, props)}
-    }, [userDetails.role, userDetails, props, setStoreUserDetails]);
+  const {
+    setStoreUserDetails
+  } = props;
 
-    useEffect(() => {
-        if (localStorage.token && !userDetails.role){sendFetchRequestIsThereLoginUser({setUserDetails})}
-    }, [userDetails.role]);
+  //Here we create reducer
+  const checkLoginUser = (state, action) => {
+    switch (action.type) {
+      case "LOGIN_SUCCESS":
+        return {
+          ...state,
+          uuid: action.fetchResults.userUUID,
+          role: action.fetchResults.role
+        };
+      case "LOGIN_FAILED":
+        return {
+          ...state,
+          isError: true
+        };
+      case "CHECK_LOGIN_USER_FAILED":
+        return state;
+      case "CLOSE_ERROR_MASSAGE":
+        return {
+          ...state,
+          isError: false
+        }
+    }
+  };
+  const initialState = {
+    uuid: null,
+    role: null,
+    isError: false
+  };
+  const init = (initialState) => initialState;
+  const [userDetails, dispatchUserState] = useReducer(checkLoginUser, initialState, init);
 
-    useEffect(() => {
-        setShowWarningMessage(error["isError"]);
-    }, [error.isError, error]);
+  useEffect(() => {
+    setStoreUserDetails(userDetails);
+    if (userDetails.role){redirectByRole(userDetails.role, props)}
+  }, [userDetails.role, userDetails, props, setStoreUserDetails]);
 
-    //Main HTML return
-    return (
-        <Container>
-            <AlertMessage
-                show={showWarningMessage}
-                onClose={() => {setStoreError({isError: false, responseStatus: null})}}
-                message="Wrong Login Details"
-                type="error"
-            />
-            <FormForInputUserInformation
-                {...props}
-                fetchRequest        ={(userDetails) => {
-                    sendFetchRequestLoginUser(
-                        userDetails,
-                        {setUserDetails},
-                        {ifCatchSetErrorInStore: (error) => {setStoreError(error)}})
-                }}
-                submitButtonTitle   ="Log In"
-                showEmailForm       ={true}
-                showPasswordForm    ={true}
-                showRoleForm        ={false}
-                showFirstNameForm   ={false}
-                showLastNameForm    ={false}
-                showLicenceForm     ={false}
-                showPhotoURLForm    ={false}
-            />
-        </Container>
-    );
+  useEffect(() => {
+    const sendFetch = async () => {
+      if (localStorage.token && !userDetails.role){
+        try {
+          const response = await fetch(
+            URLs.GET_DETAILS_BY_TOKEN,
+            {
+              method: 'GET',
+              body: null,
+              headers: {'Authorization': localStorage.token}
+            }
+          );
+          const result = await response.json();
+
+          dispatchUserState({
+            type: "LOGIN_SUCCESS",
+            fetchResults: {
+              userUUID: result.userUUID,
+              role: result.role
+            }
+          })
+        }catch (e) {
+          dispatchUserState({type: "CHECK_LOGIN_USER_FAILED"})
+        }
+      }
+    };
+    sendFetch();
+  }, [userDetails.role]);
+
+  const sendFetchForLoginUser = async (loginDetails) => {
+    try {
+      const body = JSON.stringify({
+        "email":loginDetails.email,
+        "password":loginDetails.password
+      });
+
+      const init = {
+        method: 'POST',
+        body: body,
+        headers: {},
+      };
+
+      const response = await fetch(
+        URLs.LOGIN_USER,
+        init
+      );
+
+      const result = await response.json();
+
+      localStorage.setItem("token", result.token);
+      dispatchUserState({
+        type: "LOGIN_SUCCESS",
+        fetchResults: {
+          userUUID: result.userUUID,
+          role: result.role
+        }
+      })
+    } catch (e) {
+      dispatchUserState({type: "LOGIN_FAILED"});
+    }
+  };
+
+  //Main HTML return
+  return (
+    <Container>
+      <AlertMessage
+        show={userDetails.isError}
+        onClose={() => {dispatchUserState({type: "CLOSE_ERROR_MASSAGE"})}}
+        message="Wrong Login Details"
+        type="error"
+      />
+      <FormForInputUserInformation
+        {...props}
+        fetchRequest        ={(loginDetails) => {sendFetchForLoginUser(loginDetails)}}
+        submitButtonTitle   ="Log In"
+        showEmailForm       ={true}
+        showPasswordForm    ={true}
+        showRoleForm        ={false}
+        showFirstNameForm   ={false}
+        showLastNameForm    ={false}
+        showLicenceForm     ={false}
+        showPhotoURLForm    ={false}
+      />
+    </Container>
+  );
 };
 
 export default LoginPage
--- Front/clinicfront/src/components/MainComponents/LoginPage/SetLoginPage.js
+++ Front/clinicfront/src/components/MainComponents/LoginPage/SetLoginPage.js
@@ -1,8 +1,6 @@
 import {connect} from "react-redux";
 import LoginPage from "./LoginPage";
 import {setStoreError, setStoreUserDetails} from "../../../actions";
-import {sendRequestByGivenDetails} from "../../../actions/fetchRequest";
-import {URLs} from "../../../URLs";
 
 const getError = state => (state.error);
 
@@ -21,67 +19,3 @@ export default connect(
 )(LoginPage)
 
 //CSS Stylesheet
-
-//Content for fetch request
-export const sendFetchRequestLoginUser = (loginDetails, {setUserDetails}, {ifCatchSetErrorInStore}) => {
-    const url = URLs.LOGIN_USER;
-
-    const method = 'POST';
-
-    const body = {
-        "email":loginDetails.email,
-        "password":loginDetails.password
-    };
-
-    const headers = ({});
-
-    const setInStateFunction = (responseData) => {
-        setUserDetails({
-            uuid: responseData.userUUID,
-            role: responseData.role
-        })
-    };
-
-    const specialFunction = (responseData) => {
-        localStorage.setItem("token", responseData.token)
-    };
-
-    sendRequestByGivenDetails(
-        url,
-        method,
-        body,
-        headers,
-        setInStateFunction,
-        specialFunction,
-        ifCatchSetErrorInStore
-    )
-};
-
-export const sendFetchRequestIsThereLoginUser = ({setUserDetails}) => {
-    const url = URLs.GET_DETAILS_BY_TOKEN;
-
-    const method = 'GET';
-
-    const body = null;
-
-    const headers = {'Authorization': localStorage.token};
-
-    const setInStateFunction = (responseData) => {
-        setUserDetails({
-            uuid: responseData.userUUID,
-            role: responseData.role
-        })
-    };
-
-    const specialFunction = null;
-
-    sendRequestByGivenDetails(
-        url,
-        method,
-        body,
-        headers,
-        setInStateFunction,
-        specialFunction
-    )
-
-};
