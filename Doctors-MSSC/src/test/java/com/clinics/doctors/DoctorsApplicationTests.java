59e919f38861cd1df4160d23957b4305ccc4ef1f
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-05-30 23:50:51 +0200

    init

--- Doctors-MSSC/src/test/java/com/clinics/doctors/DoctorsApplicationTests.java
+++ Doctors-MSSC/src/test/java/com/clinics/doctors/DoctorsApplicationTests.java
@@ -1,13 +1,35 @@
 package com.clinics.doctors;
 
+import com.clinics.doctors.ui.controller.DoctorController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DoctorsApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	@Autowired
+	private DoctorController doctorController;
 
 	@Test
 	void contextLoads() {
+		assertThat(doctorController).isNotNull();
+	}
+
+	@Test
+	public void helloWorld() {
+		assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/doctors/test", String.class))
+				.contains("Hello world");
 	}
 
 }
